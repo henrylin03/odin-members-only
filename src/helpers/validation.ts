f6e7d7b476014f93abe89da272239f6c852f7ee1
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-10 09:28:23 +1100

    feat: add validation of character lengths for first name, last name and username (email address) on registration form. username also has a isEmail check.
    checking for length also checks whether it's filled in or not. ideally, we also have client-side validation on top of this, but there is some checks on required-attributed fields already in native html, which i won't remove. password will need to be handled separately as i foresee some complexities

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,3 +1,22 @@
-const validateRegistrationForm = () => {};
+import { body } from "express-validator";
+
+const validateRegistrationForm = [
+	body("firstName")
+		.trim()
+		.isLength({ min: 1, max: 255 })
+		.withMessage("First name must be between 1 and 255 characters"),
+
+	body("lastName")
+		.trim()
+		.isLength({ min: 1, max: 255 })
+		.withMessage("Last name must be between 1 and 255 characters"),
+
+	body("username")
+		.trim()
+		.isLength({ min: 1, max: 255 })
+		.withMessage("Email must be between 1 and 255 characters")
+		.isEmail()
+		.withMessage("Please enter a valid email address"),
+];
 
 export { validateRegistrationForm };
