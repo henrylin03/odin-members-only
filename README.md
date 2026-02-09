59fcfce6ac1b728d45031b5b5150852c61b805b2
Henry <83106787+henrylin03@users.noreply.github.com>
2026-02-09 14:37:09 +1100

    batman

---
+++ README.md
@@ -0,0 +1,4 @@
+# Members Only
+Exclusive clubhouse where members can write anonymous posts. Inside the clubhouse, members can see who the author of a post is, but outside they can only see the story and wonder who wrote it.
+
+Part of The Odin Project's [NodeJS Course](https://www.theodinproject.com/lessons/node-path-nodejs-members-only) to practise authentication.
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import path from "node:path";
+import express from "express";
+
+const app = express();
+
+const currentPath = import.meta.dirname;
+
+app.use(express.static(path.join(currentPath, "..", "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(currentPath, "views"));
+
+/* middleware to parse data in request body */
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_req, res) => res.render("index"));
+
+const PORT = 3000;
+app.listen(PORT, (err) => {
+	if (err) throw Error;
+	console.log(`Listening on port ${PORT}`);
+});
