e24d712e96bfa7df3fd6b9bd6d9c70690d3c9ba0
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-10 14:26:50 +1100

    feat(validation): add min and max character limits for passwords
    followed OWASP's guidance for max character limits: https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html#implement-proper-password-strength-controls . min character limits set because this is just a test project. weak password is still allowed, and i dont want to make it too onerous on the user

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -18,12 +18,18 @@ const validateRegistrationForm = [
 		.isLength({ min: 1, max: 255 })
 		.withMessage(`Last name ${CHAR_LENGTH_ERROR_255}`),
 
+	// TODO: check if username already exists in database. if yes, prompt user to sign in instead
 	body("username")
 		.trim()
 		.isEmail()
 		.withMessage("Please enter a valid email address")
 		.isLength({ min: 1, max: 255 })
 		.withMessage(`Email ${CHAR_LENGTH_ERROR_255}`),
+
+	// TODO: add password strength meter: https://github.com/zxcvbn-ts/zxcvbn
+	body("password")
+		.isLength({ min: 8, max: 64 })
+		.withMessage("Password must be between 8 and 64 characters"),
 ];
 
 export { validateRegistrationForm };
