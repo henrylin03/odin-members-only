39156e55e2d3c312aa7769bb4ef52a627b2e3317
Henry <83106787+henrylin03@users.noreply.github.com>
2026-02-11 10:10:21 +1100

    feat: setup session and session storage (#9)
    * chore: install express-session
    
    * chore: install connect-pg-simple
    
    this is to create a session store for express-sessions
    
    * chore: install express-session and connect-pg-simple types
    
    silences typescript errors
    
    * feat(app.ts): set up session
    
    read through docs on why we should use resave and saveUninitialized as false here: https://github.com/expressjs/session. afaik, these should be both set as false. there is a typescript error for the secret because it's being brought in from .env file (a string) which i'll need to fix.
    
    * fix(app.ts): cookie secret typescript error
    
    throw error if cookieSecret is not found. this means that it'll pass the typescript check when it's passed into app.use(session({...}))
    
    * feat(app): create pgPool session, including session store, using the pg pool. set cookie expiry to 30 days. creates the table if missing.
    
    this session store is for prod and needed to be set up separately to just express-sessions. using this as we're building in postgres (pg library), and was suggested by odin project lesson. from stackoverflow (https://stackoverflow.com/a/47765201/16795175), it would appear that using the pool replaces the need to _also_ put in the conString (connection string to db).

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import path from "node:path";
+import connectPgSimple from "connect-pg-simple";
+import "dotenv/config";
 import express from "express";
+import session from "express-session";
+import { pool as pgPool } from "./db/pool.js";
 import indexRouter from "./routers/indexRouter.js";
 import loginRouter from "./routers/loginRouter.js";
 import registerRouter from "./routers/registerRouter.js";
@@ -13,9 +17,31 @@ app.use(express.static(path.join(currentPath, "..", "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(currentPath, "views"));
 
-/* middleware to parse data in request body */
+/* middleware */
+const cookieSecret = process.env.COOKIE_SECRET;
+if (!cookieSecret) throw new Error("COOKIE_SECRET env variable is required.");
+
+const pgSession = connectPgSimple(session);
+
+app.use(
+	session({
+		store: new pgSession({
+			pool: pgPool,
+			tableName: "user_session",
+			createTableIfMissing: true,
+		}),
+		secret: cookieSecret,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 30 * 24 * 60 * 60 * 1000,
+		}, // 30 days
+	}),
+);
+
 app.use(express.urlencoded({ extended: true }));
 
+/* routes */
 app.use("/", indexRouter);
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
