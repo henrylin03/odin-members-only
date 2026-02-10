f3459465a90820e314376d0193abf18054935aa7
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-11 09:45:00 +1100

    feat(app): create pgPool session, including session store, using the pg pool. set cookie expiry to 30 days. creates the table if missing.
    this session store is for prod and needed to be set up separately to just express-sessions. using this as we're building in postgres (pg library), and was suggested by odin project lesson. from stackoverflow (https://stackoverflow.com/a/47765201/16795175), it would appear that using the pool replaces the need to _also_ put in the conString (connection string to db).

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import path from "node:path";
-import pgSession from "connect-pg-simple";
+import connectPgSimple from "connect-pg-simple";
 import "dotenv/config";
 import express from "express";
 import session from "express-session";
+import { pool as pgPool } from "./db/pool.js";
 import indexRouter from "./routers/indexRouter.js";
 import loginRouter from "./routers/loginRouter.js";
 import registerRouter from "./routers/registerRouter.js";
@@ -20,13 +21,24 @@ app.set("views", path.join(currentPath, "views"));
 const cookieSecret = process.env.COOKIE_SECRET;
 if (!cookieSecret) throw new Error("COOKIE_SECRET env variable is required.");
 
+const pgSession = connectPgSimple(session);
+
 app.use(
 	session({
+		store: new pgSession({
+			pool: pgPool,
+			tableName: "user_session",
+			createTableIfMissing: true,
+		}),
 		secret: cookieSecret,
 		resave: false,
 		saveUninitialized: false,
+		cookie: {
+			maxAge: 30 * 24 * 60 * 60 * 1000,
+		}, // 30 days
 	}),
 );
+
 app.use(express.urlencoded({ extended: true }));
 
 /* routes */
