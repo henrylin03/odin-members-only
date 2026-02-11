2cb1712062833e4cd8b4cbcb817e3b22144ed1d2
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-11 14:59:26 +1100

    feat(login): if username is not found, error type is 'username'. if username was found but password incorrect, error type is 'password'.
    this is in the GET controller in loginController, because we will use this data to pass through error type and messages to the view, to render UI differently.

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,8 +1,20 @@
 import type { Request, Response } from "express";
 import passport from "@/config/passport.js";
 
+type LoginField = "username" | "password";
+
 const loginGet = async (req: Request, res: Response) => {
 	console.log("req.session:", req.session);
+
+	const { session } = req;
+	if (!("flash" in session) || session.flash.error.length === 0)
+		return res.render("pages/loginForm");
+
+	const fieldWithError: LoginField = session.flash.error.pop();
+	console.log(fieldWithError);
+
+	req.session.flash.error = [];
+
 	res.render("pages/loginForm");
 };
 
